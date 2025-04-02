13b537e4197a013f8e6ad635e41e4f283f0de4f7
Victor_Rafael_Lima_De_Santana <victorrafaellimadesantana14@gmail.com>
2025-04-02 12:44:01 -0300

    Add files via upload

---
+++ Recursividade em C/Questão2 RecursividadeC.c
@@ -0,0 +1,32 @@
+/*Questão 2 Recursividade: Calcular a potência de um número elevado a um expoente usando recursividade. */
+
+#include <stdio.h> 
+#include <locale.h>
+
+int potencia(int base, int expoente) {
+	if (expoente == 0) {
+	return 1;
+	}else{
+	return base * potencia(base, expoente - 1); 
+    } 
+} 
+ 
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	int base, expoente; 
+ 
+    printf("Digite a base: ");
+	scanf("%d", &base); 
+ 
+    printf("Digite o expoente: ");
+	scanf("%d", &expoente); 
+ 
+    if (expoente < 0){
+        printf("Erro: Não é permitido expoente negativo.\n"); 
+    }else{ 
+        printf("%d elevado a %d é %d.\n", base, expoente, potencia(base, expoente)); 
+    } 
+    return 0; 
+} 
+
